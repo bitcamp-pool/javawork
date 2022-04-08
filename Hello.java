453a7a1cc1fb0ccbdae794bc99dc83949cd31f19
bitcamp <call121233@naver.com>
2022-04-08 16:54:07 +0900

    20220408 16:53

---
+++ Hello.java
@@ -0,0 +1,7 @@
+class Hello{
+	public static void main(String[] args){
+		System.out.println("직접 클래스 생성!!");
+		System.out.println("0번 값:" + args[0]);
+		System.out.println("1번 값:" + args[1]);
+	}
+}
---
+++ JavaProject/src/day0407/Ex09ListFile.java
@@ -0,0 +1,8 @@
+package day0407;
+
+public class Ex09ListFile {
+	public static void main(String[] args) {
+		
+		PersonManager pm = new PersonManager();
+	}
+}
