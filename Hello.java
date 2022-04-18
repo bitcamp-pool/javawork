5cd865b0f98588ee8bfc6560f327e9de55aa00b9
bitcamp <call121233@naver.com>
2022-04-18 20:00:25 +0900

    20220418 20:00

--- Hello.java
+++
@@ -1,7 +0,0 @@
-class Hello{
-	public static void main(String[] args){
-		System.out.println("직접 클래스 생성!!");
-		System.out.println("0번 값:" + args[0]);
-		System.out.println("1번 값:" + args[1]);
-	}
-}
