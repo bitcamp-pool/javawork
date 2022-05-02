181a37c7eaa663e4aeb7740ead6f5b78f47d1e97
bitcamp <call121233@naver.com>
2022-05-02 18:30:50 +0900

    20220502 18:30

--- SpringBootEx05/src/main/java/com/example/demo/SpringBootEx05Application.java
+++ SpringBootEx05/src/main/java/com/example/demo/SpringBootEx05Application.java
@@ -2,8 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan({"com.example.demo", " ", " "})
+@ComponentScan({"com.example.demo", "test.*"})
 public class SpringBootEx05Application {
 
 	public static void main(String[] args) {
