856b1d2739fdeee586294c665f1016f03138b4d0
bitcamp-pool <85716998+bitcamp-pool@users.noreply.github.com>
2024-10-09 22:57:17 +0900

    Update oasis-layout.cc

--- files/oasis-layout.cc
+++ files/oasis-layout.cc
@@ -1,4 +1,3 @@
-​
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
@@ -8,46 +7,77 @@
 #include "misc/utils.h"
 #include "creator.h"
 #include "parser.h"
-#include <iostream>
 #include "layoutbuilder.h"
 
-    using namespace std;
+#include "iostream"
+
+using namespace std;
 using namespace SoftJin;
 using namespace Oasis;
 
-const char UsageMessage[] =
+const char  UsageMessage[] =
     "usage:  %s [-c cellname] [-ilntvxzs] input-oasis-file output-oasis-file\n"
     "Options:\n"
-    "  -c cellname  Select cell(s). Specify the name(s) of the cell(s) to process.\n"
-    "  -i  Write name records immediately to the file.\n"
-    "  -l  Ignore LAYERNAME records.\n"
-    "  -n  Do not insist on strict conformance to the OASIS specification.\n"
-    "  -t  Ignore TEXT and TEXTSTRING records.\n"
-    "  -v  Ignore the validation scheme and signature in the END record.\n"
-    "  -x  Ignore XNAME, XELEMENT, and XGEOMETRY records.\n"
-    "  -z  Disable compression for the output file.\n"
-    "  -s  Disable strict mode.\n"
+    "    -c cellname   Select cell(s). Specify the name(s) of the cell(s) to process.\n"
+    "    -i            Write name records immediately to the file.\n"
+    "    -l            Ignore LAYERNAME records.\n"
+    "    -n            Do not insist on strict conformance to the OASIS specification.\n"
+    "    -t            Ignore TEXT and TEXTSTRING records.\n"
+    "    -v            Ignore the validation scheme and signature in the END record.\n"
+    "    -x            Ignore XNAME, XELEMENT, and XGEOMETRY records.\n"
+    "    -z            Disable compression for the output file.\n"
+    "    -s            Disable strict mode.\n"
     "Notes:\n"
-    "  - The input and output files must have the '.oas' extension.\n";
+    "    - The input and output files must have the '.oas' extension.\n";
 
-static void UsageError()
-{
+static void UsageError() {
     fprintf(stderr, UsageMessage, GetProgramName());
     exit(1);
 }
 
-static void DisplayWarning(const char *msg)
-{
+static void DisplayWarning(const char* msg) {
     Error("%s", msg);
 }
 
-bool isOasisFile(const std::string &str)
-{
+bool isOasisFile(const std::string& str) {
     return str.size() > 4 && str.substr(str.size() - 4) == ".oas";
 }
 
-int main(int argc, char *argv[])
-{
+// 메뉴 표시 함수
+void displayMenu() {
+    std::cout << endl;
+    std::cout << "[Menu]\n";
+    std::cout << "1. Print BBox of a Specific TOP CELL\n";
+    std::cout << "2. Print BBoxes of All Cells\n";
+    std::cout << "3. Exit\n";
+    std::cout << "Enter your choice: ";
+}
+
+// 특정 TOP CELL의 BBox 출력 함수
+void printSpecificTopCellBBox(JLayoutBuilder& layoutBuilder) {
+    std::string cellName;
+    std::cout << "Enter the TOP CELL name: ";
+    std::cin >> cellName;
+
+    JCell* cell = layoutBuilder.findRefCell(new CellName(cellName));
+    if (cell) {
+        std::unordered_set<const JCell*> visited;
+        JLayout::BBox bbox = layoutBuilder.calculateCellBBox(cell, visited);
+        std::cout << "BBox for TOP CELL " << cellName << ": ("
+                  << bbox.x_min / 1000.0 << ", " << bbox.y_min / 1000.0 << ") ("
+                  << bbox.x_max / 1000.0 << ", " << bbox.y_max / 1000.0 << ")\n";
+    } else {
+        std::cout << "TOP CELL " << cellName << " not found.\n";
+    }
+}
+
+// 전체 셀의 BBox 출력 함수
+void printAllCellBBoxes(const JLayoutBuilder& layoutBuilder) {
+    layoutBuilder.printLayoutInfo();
+}
+
+
+int main(int argc, char* argv[]) {
     SetProgramName(argv[0]);
 
     std::vector<std::string> enteredCellNames;
@@ -57,48 +87,27 @@ int main(int argc, char *argv[])
 
     int opt;
     opterr = 0;
-    while ((opt = getopt(argc, argv, "c:lntvxizs")) != EOF)
-    {
-        switch (opt)
-        {
-        case 'c':
-        {
-            // 첫 번째 셀 이름 추가
-            enteredCellNames.emplace_back(optarg);
-            // 나머지 셀 이름 추가
-            while (optind < argc && argv[optind][0] != '-' && !isOasisFile(argv[optind]))
-            {
-                enteredCellNames.emplace_back(argv[optind]);
-                ++optind;
+    while ((opt = getopt(argc, argv, "c:lntvxizs")) != EOF) {
+        switch (opt) {
+            case 'c': {
+                // 첫 번째 셀 이름 추가
+                enteredCellNames.emplace_back(optarg);
+                // 나머지 셀 이름 추가
+                while (optind < argc && argv[optind][0] != '-' && !isOasisFile(argv[optind])) {
+                    enteredCellNames.emplace_back(argv[optind]);
+                    ++optind;
+                }
+                break;
             }
-            break;
-        }
-        case 'l':
-            parserOptions.wantLayerName = false;
-            break;
-        case 'n':
-            parserOptions.strictConformance = false;
-            break;
-        case 't':
-            parserOptions.wantText = false;
-            break;
-        case 'v':
-            parserOptions.wantValidation = false;
-            break;
-        case 'x':
-            parserOptions.wantExtensions = false;
-            break;
-        case 'i':
-            creatorOptions.immediateNames = true;
-            break;
-        case 'z':
-            creatorOptions._mustCompressed = false;
-            break;
-        case 's':
-            creatorOptions._mustStrict = false;
-            break;
-        default:
-            UsageError();
+            case 'l':  parserOptions.wantLayerName     = false;   break;
+            case 'n':  parserOptions.strictConformance = false;   break;
+            case 't':  parserOptions.wantText          = false;   break;
+            case 'v':  parserOptions.wantValidation    = false;   break;
+            case 'x':  parserOptions.wantExtensions    = false;   break;
+            case 'i':  creatorOptions.immediateNames   = true;    break;
+            case 'z':  creatorOptions._mustCompressed  = false;   break;
+            case 's':  creatorOptions._mustStrict      = false;   break;
+            default:   UsageError();
         }
     }
 
@@ -106,43 +115,51 @@ int main(int argc, char *argv[])
     creatorOptions._hasCellNames = isCellNames;
 
     // 마지막 두 개의 인자는 입력 및 출력 파일로, 반드시 .oas 확장자여야 함
-    if (optind != argc - 2 || !isOasisFile(argv[optind]) || !isOasisFile(argv[optind + 1]))
-    {
+    if (optind != argc - 2 || !isOasisFile(argv[optind]) || !isOasisFile(argv[optind + 1])) {
         UsageError();
     }
 
-    const char *infilename = argv[optind];
-    const char *outfilename = argv[optind + 1];
+    const char* infilename = argv[optind];
+    const char* outfilename = argv[optind + 1];
 
-    try
-    {
+    try {
         OasisParser parser(infilename, DisplayWarning, parserOptions);
         OasisCreator creator(outfilename, creatorOptions);
         JLayoutBuilder layoutBuilder(creator);
 
-        // 셀 이름이 지정되지 않은 경우 전체 파일 파싱
-        if (!isCellNames)
-        {
-            parser.parseFile(&layoutBuilder);
-        }
-        else
-        {
-            // 지정된 셀 이름이 있는 경우
-            if (!parser.JCreateLayoutDataBase(enteredCellNames, &layoutBuilder))
-            {
-                FatalError("file '%s' has no cell name you entered.", infilename);
+        parser.parseFile(&layoutBuilder);
+
+        int choice;
+        do {
+            displayMenu();
+            std::cin >> choice;
+
+            // 입력 오류 처리
+            if (std::cin.fail()) {
+                std::cin.clear();  // 오류 플래그 초기화
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // 입력 버퍼 비우기
+                std::cout << "Invalid input. Please enter a valid option.\n";
+                continue;
             }
-        }
 
-        // layoutBuilder.generateBinary(); // binary 생성
-
-        if (fclose(stdout) == EOF)
-        {
-            FatalError("cannot close standard output: %s", strerror(errno));
-        }
-    }
-    catch (const std::exception &exc)
-    {
+            switch (choice) {
+            case 1:
+                std::cout << endl;
+                printSpecificTopCellBBox(layoutBuilder);
+                break;
+            case 2:
+                std::cout << endl;
+                printAllCellBBoxes(layoutBuilder);
+                break;
+            case 3:
+                std::cout << "Exiting the program.\n";
+                break;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+                break;
+            }
+        } while (choice != 3);
+    } catch (const std::exception& exc) {
         FatalError("%s", exc.what());
     }
 
