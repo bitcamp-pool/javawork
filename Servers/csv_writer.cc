fbed5b883e5307049b0c53127414217f29b30f7c
bitcamp-pool <85716998+bitcamp-pool@users.noreply.github.com>
2024-10-25 00:38:55 +0900

    Update csv_writer.cc

--- Servers/csv_writer.cc
+++ Servers/csv_writer.cc
@@ -1,16 +1,34 @@
 #include "csv_writer.h"
 #include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+using namespace Oasis;
 
 CSVWriter::CSVWriter(const std::string& filename) : filename(filename) {}
 
-void CSVWriter::write(const Statistics& stats) {
+void CSVWriter::StatisticsWrite(const Statistics& stats) {
     std::ofstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open CSV file.");
+    }
+
     file << "name,value\n";
-    file << "total_cells," << stats.totalCells << "\n";
-    file << "total_layers," << stats.totalLayers << "\n";
-    file << "total_shapes," << stats.totalShapes << "\n";
-    file << "total_placements," << stats.totalPlacements << "\n";
-    file << "cell_with_max_refs," << stats.cellWithMaxRefs << "\n";
-    file << "cell_with_max_shapes," << stats.cellWithMaxShapes << "\n";
+    file << "filename," << stats.filename << "\n";
+    file << "TAT(s)," << stats.TAT << "\n";
+    file << "filesize(MB)," << stats.fileSizeMB << "\n";
+    file << "#cells," << stats.totalCells << "\n";
+    file << "#layers," << stats.totalLayers << "\n";
+    file << "#shapes," << stats.totalShapes << "\n";
+    file << "#shapes_expand," << stats.totalShapesExpand << "\n";
+    file << "#refs," << stats.totalPlacements << "\n";
+    file << "#refs_expand," << stats.totalPlacementsExpand << "\n";
+
+    for (int i = 0; i < 12; ++i) {
+        file << "#srep_" << i << "," << stats.srepCounts[i] << "\n";
+        file << "#srep_" << i << "_expand," << stats.srepExpandCounts[i] << "\n";
+    }
+
     file.close();
 }
+
